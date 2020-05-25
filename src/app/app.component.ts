f07452ff87135609e1273f794ecfcbb984f8d8dd
rcfernandez <rcfernandez@outlook.com.ar>
2020-05-24 21:38:28 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // nombre del componente, sera llamado con <app-root></app-root>
+  templateUrl: './app.component.html',  // HTML del componente
+  styleUrls: ['./app.component.css']    // CSS del componente
 })
+
+
 export class AppComponent {
   title = 'pwa2020';
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './prueba/prueba.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { FormsModule } from '@angular/forms' // *2 hay que agregarlo para que funcione el 'ngModel'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule  // *2 hay que agregarlo para que funcione el 'ngModel'
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,   // este es el default
+    // PruebaComponent, // *1 para que empiece con este modulo
+  ]
 })
 export class AppModule { }
